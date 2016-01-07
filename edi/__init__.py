2c0bed7db9f46a7b1c96012829de4753789ec829
John Reese <john@noswap.com>
2016-01-06 18:51:32 -0800

    Module structure

---
+++ edi/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2016 John Reese
+# Licensed under the MIT license
+
+'''Simple and elegant Slack bot.'''
+
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from . import edi
+
+setup(name='Edi',
+      description='Simple and elegant Slack bot',
+      version=edi.__version__,
+      author='John Reese',
+      author_email='john@noswap.com',
+      url='https://github.com/jreese/edi',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Utilities',
+                   'Development Status :: 4 - Beta',
+                   ],
+      license='MIT License',
+      packages=['edi'],
+      package_data={'edi': ['defaults.yaml']}
+      requires=['ent>=0.3.0', 'slacker'],
+      #scripts=['bin/edi],
+      )
