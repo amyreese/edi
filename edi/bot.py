25c12888c1cf877ca72c9967c73f08f462b25b17
John Reese <john@noswap.com>
2016-03-31 20:20:21 -0700

    Prototype asyncio event loop based on tasks
    Depends on async/await keywords from python 3.5+ and websockets library.

--- edi/bot.py
+++ edi/bot.py
@@ -1,19 +1,34 @@
 # Copyright 2016 John Reese
 # Licensed under the MIT license
 
+import asyncio
+import signal
+import websockets
+
 from argparse import ArgumentParser
 from os import path
 from typing import List
 
+from .common import Task, PeriodicTask
 from .config import Config
 from .log import Log, init_logger
 
 
-class Edi(object):
+class SecondLoop(PeriodicTask):
+    INTERVAL = 5.0
+
+    async def run(self):
+        Log.info('second loop running')
+
+
+class Edi(Task):
 
     def __init__(self, config: Config) -> None:
         self.config = config
         self.slack = None
+        self.loop = None
+        self.running = True
+        self.stop_attempts = 0
 
         Log.debug('Edi ready')
 
@@ -21,9 +36,44 @@ class Edi(object):
     def connected(self) -> bool:
         return self.slack is not None
 
+    async def run(self) -> None:
+        Log.info('in main loop')
+        async with websockets.connect('ws://localhost:2345') as conn:
+            self.slack = conn
+            Log.info('connected')
+
+            SecondLoop.start()
+
+            counter = 0
+            while self.running and counter <= 3:
+                Log.info('sending %s', counter)
+                await conn.send(str(counter))
+                retval = await conn.recv()
+                Log.info('received %s', retval)
+                await asyncio.sleep(1.0)
+                counter += 1
+
+        self.slack = None
+        Log.info('main loop completed')
+
+    def ctrlc(self) -> None:
+        if self.connected and self.stop_attempts < 1:
+            Log.info('stopping main loop')
+            self.running = False
+            self.stop_attempts += 1
+        else:
+            Log.info('force stopping event loop')
+            self.loop.stop()
+
     def start(self) -> None:
         Log.info('starting event loop')
-        pass
+
+        self.loop = asyncio.get_event_loop()
+        self.loop.add_signal_handler(signal.SIGINT, self.ctrlc)
+        self.loop.run_until_complete(self.start_task())
+        self.loop.run_until_complete(Task.stop_all())
+
+        Log.info('event loop stopped')
 
 
 def init_from_config(config: Config) -> Edi:
